f469100514492a75fa44d0d0036ac864a9fbc571
Oskar Polak <biuro@fivarto.pl>
2018-03-20 11:07:55 +0100

    asd

--- src/main/java/pl/oskarpolak/simplychat/models/ChatSocket.java
+++ src/main/java/pl/oskarpolak/simplychat/models/ChatSocket.java
@@ -1,6 +1,7 @@
 package pl.oskarpolak.simplychat.models;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
@@ -9,9 +10,14 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 import org.springframework.web.socket.handler.BinaryWebSocketHandler;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @EnableWebSocket
 @Component
 public class ChatSocket extends TextWebSocketHandler implements WebSocketConfigurer {
+
+    private List<WebSocketSession> sessionList = new ArrayList<>();
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry webSocketHandlerRegistry) {
@@ -20,11 +26,22 @@ public class ChatSocket extends TextWebSocketHandler implements WebSocketConfigu
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-        super.handleTextMessage(session, message);
+        for (WebSocketSession webSocketSession : sessionList) {
+            webSocketSession.sendMessage(new TextMessage("log:"+message.getPayload()));
+        }
     }
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-        System.out.println("Połączono!!!!!!!");
+        sessionList.add(session);
+
+        for (WebSocketSession webSocketSession : sessionList) {
+            webSocketSession.sendMessage(new TextMessage("connected:Ktoś sie polaczyl"));
+        }
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        sessionList.remove(session);
     }
 }
