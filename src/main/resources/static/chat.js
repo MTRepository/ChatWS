d60374368a036f0b98b6b9df1eac75588760fc96
Oskar Polak <biuro@fivarto.pl>
2018-03-20 13:09:31 +0100

    test

--- src/main/resources/static/chat.js
+++ src/main/resources/static/chat.js
@@ -1,6 +1,6 @@
 
 function onLoad() {
-    var wsUri = "ws://akademiachat.herokuapp.com//chat";
+    var wsUri = "ws://akademiachat.herokuapp.com/chat";
     websocket = new WebSocket(wsUri);
     websocket.onopen = function(evt) { onOpen(evt) };
     websocket.onclose = function(evt) { onClose(evt) };
