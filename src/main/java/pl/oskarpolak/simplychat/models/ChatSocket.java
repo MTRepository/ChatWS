3bf65af6e15aa93a4109c8b4dbb127b1f2365fa3
Oskar Polak <biuro@fivarto.pl>
2018-03-20 10:28:49 +0100

    Initial commit

---
+++ src/main/java/pl/oskarpolak/simplychat/models/ChatSocket.java
@@ -0,0 +1,28 @@
+package pl.oskarpolak.simplychat.models;
+
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.handler.BinaryWebSocketHandler;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+@EnableWebSocket
+public class ChatSocket extends TextWebSocketHandler implements WebSocketConfigurer {
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry webSocketHandlerRegistry) {
+        webSocketHandlerRegistry.addHandler(this, "/chat").setAllowedOrigins("*");
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        super.handleTextMessage(session, message);
+    }
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        System.out.println("Połączono!!!!!!!");
+    }
+}
